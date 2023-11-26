a9c5668f176ab846fd463ce0317879c62a460124
dipaloke <dipalokebiswas96@gmail.com>
2023-11-26 11:28:26 +0600

    created card, chart, rightbar, transection components for dashboard

---
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./transactions.module.css"
+
+const Transaction = () => {
+  return (
+    <div>Transaction</div>
+  )
+}
+
+export default Transaction
