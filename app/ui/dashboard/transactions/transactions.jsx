d5b7a1f6bd61e1f45bfb6e87538dad2edd50939b
dipaloke <dipalokebiswas96@gmail.com>
2023-11-26 18:13:24 +0600

    Finished creating transection section of dashboard, contains name, status, date, amount

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,10 +1,107 @@
-import React from 'react'
-import styles from "./transactions.module.css"
+import React from "react";
+import styles from "./transactions.module.css";
+import Image from "next/image";
 
 const Transaction = () => {
   return (
-    <div>Transaction</div>
-  )
-}
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user avater"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Dipaloke Biswas
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>
+                pending
+              </span>
+            </td>
+            <td>14.02.2024</td>
+            <td>$5.20</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user avater"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.done}`}>Done</span>
+            </td>
+            <td>15.03.2024</td>
+            <td>$7.50</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user avater"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>12.05.2024</td>
+            <td>$10.23</td>
+          </tr>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt="user avater"
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                Dipaloke Biswas
+              </div>
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.cancelled}`}>
+                Cancelled
+              </span>
+            </td>
+            <td>12.05.2024</td>
+            <td>$10.23</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Transaction
+export default Transaction;
