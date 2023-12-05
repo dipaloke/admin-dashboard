a1cea4bc34d611037de82a826cd7da7b6c7bee1f
dipaloke <dipalokebiswas96@gmail.com>
2023-12-05 13:25:30 +0600

    configured images taken from pixels website. (images.pexels.com)

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -29,36 +29,38 @@ const UsersPage = async () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt="user avatar"
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>john@gmail.com</td>
-            <td>13.05.2024</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src= {user.img || "/noavatar.png"}
+                    alt="user avatar"
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>john@gmail.com</td>
+              <td>13.05.2024</td>
+              <td>Admin</td>
+              <td>active</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/users/test">
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
                   </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
